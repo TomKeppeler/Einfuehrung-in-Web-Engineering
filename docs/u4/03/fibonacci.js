695108d5fd8d4b60ac06d8db72e239bc2ad3e237
Tom Keppeler <49951477+TomKeppeler@users.noreply.github.com>
2022-11-23 16:24:40 +0100

    Update fibonacci.js

--- docs/u4/03/fibonacci.js
+++ docs/u4/03/fibonacci.js
@@ -3,11 +3,11 @@ const fibonacci = (n, memo = []) => {
     if(n < 2) return 1;
     memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
     return memo[n];
-}
+};
 
 ret = "";
 i = 0;
-while(i <= 200) {
+while(i <= 2000) {
     f = fibonacci(i);
     i++;
     ret += i + ": " + f + "\n";
