f0b4ce8b65f23f97c577351e6a674f07d0a3bfda
Tom Keppeler <49951477+TomKeppeler@users.noreply.github.com>
2022-12-12 17:58:50 +0100

    arbeit

--- docs/u5/02/module.js
+++ docs/u5/02/module.js
@@ -1,86 +1,35 @@
-window.onload = function () {
-
-    var seconds = 0o0;
-    var tens = 0o0;
-    var appendTens = document.getElementById("tens")
-    var appendSeconds = document.getElementById("seconds")
-    var buttonStart = document.getElementById('button-start');
-    var buttonStop = document.getElementById('button-stop');
-    var buttonReset = document.getElementById('button-reset');
-    var Interval ;
-    var button = [];
-    var buttonIndex = 0;
-
-    buttonStart.onclick = function() {
-
-        clearInterval(Interval);
-        Interval = setInterval(startTimer, 10);
+class Presenter {
+    constructor(name, minuten, secounds, button) {
+        this.name = name;
+        this.minuten = minuten;
+        this.secounds = secounds;
+        this.button = button;
+        this.button.addEventListener("click", () => startStop(button));
     }
-
-    buttonStop.onclick = function() {
-        clearInterval(Interval);
-    }
-
-
-    buttonReset.onclick = function() {
-        clearInterval(Interval);
-        tens = "00";
-        seconds = "00";
-        appendTens.innerHTML = tens;
-        appendSeconds.innerHTML = seconds;
-    }
-
-
-
-    function startTimer () {
-        tens++;
-
-        if(tens <= 9){
-            appendTens.innerHTML = "0" + tens;
-        }
-
-        if (tens > 9){
-            appendTens.innerHTML = tens;
-
-        }
-
-        if (tens > 99) {
-            console.log("seconds");
-            seconds++;
-            appendSeconds.innerHTML = "0" + seconds;
-            tens = 0;
-            appendTens.innerHTML = "0" + 0;
-        }
-
-        if (seconds > 9){
-            appendSeconds.innerHTML = seconds;
-        }
-
-    }
-
-
-
-
-
-
-
 }
+
+
+var presenter = [];
+
+var presenterIndex = 0;
+
+
 function addPerson() {
     const presentorName = document.getElementById('input');
     if (presentorName.value !== '') {
         const list = document.getElementById('list');
         const li = document.createElement('li');
-        button[buttonIndex++] = document.createElement('button');
-        button[buttonIndex - 1].innerHTML = presentorName.value;
-        button[buttonIndex - 1].setAttribute('onclick', 'startStop(button[' + (buttonIndex - 1) + '])');
-        button[buttonIndex - 1].setAttribute('class', 'button');
-        button[buttonIndex - 1].setAttribute('id', 'button' + buttonIndex);
-        li.appendChild(presentorName.value);
-        li.appendChild(button[buttonIndex - 1]);
+        this.button[this.buttonIndex++] = document.createElement('button');
+        this.button[this.buttonIndex - 1].innerHTML = presentorName.value;
+        this.button[this.buttonIndex - 1].setAttribute('onclick', 'startStop(' + button[(buttonIndex - 1)] + ')');
+        this.button[this.buttonIndex - 1].setAttribute('class', 'button');
+        this.button[this.buttonIndex - 1].setAttribute('id', 'button' + buttonIndex);
+        this.button[this.buttonIndex - 1].innerHTML = 'Start';
+        li.append(presentorName.value);
+        li.append(" " , this.seconds, ":" , this.tens);
+        li.append(button[buttonIndex - 1]);
         list.appendChild(li);
-
-        clearInterval(Interval);
-        Interval = setInterval(startTimer, 10);
+        presentorName.value = '';
     }
 }
 
