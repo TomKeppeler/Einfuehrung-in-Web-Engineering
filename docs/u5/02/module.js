5516c7340f180fdad7bf4eb771dad9027f15d53a
Tom Keppeler <49951477+TomKeppeler@users.noreply.github.com>
2022-12-13 15:48:11 +0100

    fast fertig nur noch enter key event

--- docs/u5/02/module.js
+++ docs/u5/02/module.js
@@ -8,11 +8,10 @@ class Presenter {
         this.spanTime.innerHTML = formatTime(this.secounds, this.minuten);
         this.spanTime.setAttribute("id", "time" + this.index);
         this.button = button;
-        this.button.addEventListener("click", () => startStop(button));
-        this.button.innerHTML = "Start";
+        this.button.addEventListener("click", () => startStop(button, this.index));
+        this.button.innerHTML = "Stop";
         this.index = index;
         this.button.setAttribute("id", "startStopButton" + this.index);
-        this.startTime = new Date();
         this.interval = createInterval(this.spanTime, this.index);
     }
 
@@ -21,9 +20,8 @@ class Presenter {
         listItemElement.append(this.name, ": ", this.spanTime, " ", this.button);
         return listItemElement;
     }
-
-
 }
+
 function formatTime(seconds, minutes) {
     let viewSeconds, viewMinutes;
     if (seconds < 10) {
@@ -42,39 +40,54 @@ function formatTime(seconds, minutes) {
 let presenter = [];
 let presenterIndex = 0;
 
+function stopAllButThis(presenterIndex) {
+    for (let i = 0; i < presenter.length; i++) {
+        if (i !== presenterIndex) {
+            clearInterval(presenter[i].interval);
+            document.getElementById("startStopButton" + i).innerHTML = "Start";
+        }
+    }
+}
+
+function keyEnterEvent(key){
+    console.log(key);
+    if(key.code === 'Enter') {
+        addPerson();
+    }
+}
 
 function addPerson() {
     const presenterName = document.getElementById('input');
     if (presenterName.value !== '') {
-
-
         presenter[presenterIndex] = new Presenter(presenterName.value, 0, 0, document.createElement('button'), presenterIndex);
-
-
         document.getElementById('list')
             .appendChild(presenter[presenterIndex].appendAll());
         presenterIndex++;
         presenterName.value = '';
-
+        stopAllButThis(presenterIndex - 1);
     }
 }
 
-function startStop(element) {
+function startStop(element, index) {
     if (element.innerHTML === 'Stop') {
         element.innerHTML = 'Start';
+        clearInterval(presenter[index].interval);
     } else {
         element.innerHTML = 'Stop';
+        presenter[index].interval = createInterval(presenter[index].spanTime, index);
+        stopAllButThis(index);
     }
 }
 
 function createInterval(timeSpan, index) {
-
     return setInterval(function () {
-        let currentTime = new Date();
-        let timeDifference = currentTime - presenter[index].startTime;
-        let seconds = Math.floor((timeDifference / 1000) % 60);
-        let minutes = Math.floor((timeDifference / 1000 / 60) % 60);
-        timeSpan.innerHTML = formatTime(seconds, minutes);
+        presenter[index].secounds++;
+        if (presenter[index].secounds === 60) {
+            presenter[index].minuten++;
+            presenter[index].secounds = 0;
+        }
+
+        timeSpan.innerHTML = formatTime(presenter[index].secounds, presenter[index].minuten);
 
     }, 1000);
 }
