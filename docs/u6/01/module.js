768cc8cad2b726b0bab57da717509439ff08214a
Tom Keppeler <49951477+TomKeppeler@users.noreply.github.com>
2023-01-02 19:50:17 +0100

    reflector code structure

--- docs/u6/01/module.js
+++ docs/u6/01/module.js
@@ -1,31 +1,32 @@
 const input = document.getElementById('input');
+
 function checkKlammern() {
-  const text = input.value;
-  const klammern = [];
-  const paare = {
-    '[': ']',
-    '(': ')',
-    '{': '}'
-  };
-  let valid = true;
+    const text = input.value;
+    const klammern = [];
+    const paare = {
+        '[': ']',
+        '(': ')',
+        '{': '}'
+    };
+    let valid = true;
 
-  for (let i = 0; i < text.length; i++) {
-    const charakter = text[i];
-    if (charakter === '[' || charakter === '(' || charakter === '{') {
-      klammern.push(  charakter);
-    } else if (charakter === ']' || charakter === ')' || charakter === '}') {
-      if (klammern.length === 0 || paare[klammern.pop()] !== charakter) {
-        valid = false;
-        break;
-      }
+    for (let i = 0; i < text.length; i++) {
+        const charakter = text[i];
+        if (charakter === '[' || charakter === '(' || charakter === '{') {
+            klammern.push(charakter);
+        } else if (charakter === ']' || charakter === ')' || charakter === '}') {
+            if (klammern.length === 0 || paare[klammern.pop()] !== charakter) {
+                valid = false;
+                break;
+            }
+        }
     }
-  }
 
-  if (valid && klammern.length === 0) {
-    console.log('Die Klammern sind korrekt geschachtelt.');
-    input.style.color = 'black';
-  } else {
-    console.log('Die Klammern sind nicht korrekt geschachtelt.');
-    input.style.color = 'red';
-  }
+    if (valid && klammern.length === 0) {
+        console.log('Die Klammern sind korrekt geschachtelt.');
+        input.style.color = 'black';
+    } else {
+        console.log('Die Klammern sind nicht korrekt geschachtelt.');
+        input.style.color = 'red';
+    }
 }
