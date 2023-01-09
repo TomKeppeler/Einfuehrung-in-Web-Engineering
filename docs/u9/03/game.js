59201dd0171fb0f4084dd8c1502ac70303fd7283
Tom Keppeler <49951477+TomKeppeler@users.noreply.github.com>
2023-01-09 22:23:45 +0100

    finish 9.3
    but the reset isnt working

--- docs/u9/03/game.js
+++ docs/u9/03/game.js
@@ -1,65 +1,116 @@
-const tttTable = document.querySelector('#ttt');
-
-tttTable.addEventListener('click', onClickMethode);
+document.querySelector('#ttt').addEventListener('click', onClickMethode);
 
 let turn = true;
 
+const turnDisplay = document.getElementById('turn');
+
+const cells = [];
+cells[0] = ['', '', ''];
+cells[1] = ['', '', ''];
+cells[2] = ['', '', ''];
+
 class cell {
-    constructor (col, row) {
+    constructor(col, row) {
         this.col = col;
         this.row = row;
         this.clicked = false;
         this.owner = '';
     }
-    setOwner (owner) {
+
+    setOwner(owner) {
         this.owner = owner;
     }
-    setClicked () {
+
+    setClicked() {
         this.clicked = true;
     }
 }
-const cells = [];
-cells[0] = [false, false, false];
-cells[1] = [false, false, false];
-cells[2] = [false, false, false];
 
 
-function onClickMethode (e) {
+
+function onClickMethode(e) {
     const clickedCell = e.target;
     if (clickedCell.nodeName === 'TD' && clickedCell.textContent !== '') {
         if (turn) {
             clickedCell.className = 'xDisplay';
             clickedCell.innerHTML = '<svg><use xlink:href="#cross"/></svg>';
-        } else{
+        } else {
             clickedCell.className = 'oDisplay';
             clickedCell.innerHTML = '<svg><use xlink:href="#circle"/></svg>';
         }
         const cellPosition = clickedCell.id.split(',');
-        const cell = cells[cellPosition[0]][cellPosition[1]];
-        cell.setClicked();
-        cell.setOwner(turn ? 'X' : 'O');
+        cells[cellPosition[0]][cellPosition[1]] = turn ? 'x' : 'o';
         turn = !turn;
-        testIfWon(cell);
+        turnDisplay.innerHTML = turn ? 'X ist drann!' : 'O ist drann!';
+        testIfWon();
     }
 }
 
-function testIfWon(clickedCell) {
-    winningCombination = [
-        [cells[0][0], cells[0][1], cells[0][2]],
-        [cells[1][0], cells[1][1], cells[1][2]],
-        [cells[2][0], cells[2][1], cells[2][2]],
-        [cells[0][0], cells[1][0], cells[2][0]],
-        [cells[0][1], cells[1][1], cells[2][1]],
-        [cells[0][2], cells[1][2], cells[2][2]],
-        [cells[0][0], cells[1][1], cells[2][2]],
-        [cells[0][2], cells[1][1], cells[2][0]]
-    ];
-    for (const winningCombinationKey in winningCombination) {
-        for (const cell in winningCombinationKey) {
-            if(cell.clicked) {
+function resetGame() {
+    cells[0] = ['', '', ''];
+    cells[1] = ['', '', ''];
+    cells[2] = ['', '', ''];
+    const allCells = document.querySelectorAll('td');
+    allCells.forEach(cell => {
+        cell.className = '';
+        cell.innerHTML = '';
+    });
+    turn = true;
+    turnDisplay.innerHTML = 'X ist drann!';
+}
 
-            }
-        }
+function testIfWon() {
+    let hasWon = false;
+    if (cells[0][0] === 'x' && cells[0][1] === 'x' && cells[0][2] === 'x') { //rows x
+        alert('x won');
+        hasWon = true;
+    } else if (cells[1][0] === 'x' && cells[1][1] === 'x' && cells[1][2] === 'x') {
+        alert('x won');
+        hasWon = true;
+    } else if (cells[2][0] === 'x' && cells[2][1] === 'x' && cells[2][2] === 'x') {
+        alert('x won');
+        hasWon = true;
+    } else if (cells[0][0] === 'x' && cells[1][0] === 'x' && cells[2][0] === 'x') { //columns x
+        alert('x won');
+        hasWon = true;
+    } else if (cells[0][1] === 'x' && cells[1][1] === 'x' && cells[2][1] === 'x') {
+        alert('x won');
+        hasWon = true;
+    } else if (cells[0][2] === 'x' && cells[1][2] === 'x' && cells[2][2] === 'x') {
+        alert('x won');
+        hasWon = true;
+    } else if (cells[0][0] === 'x' && cells[1][1] === 'x' && cells[2][2] === 'x') { //diagonal x
+        alert('x won');
+        hasWon = true;
+    } else if (cells[0][2] === 'x' && cells[1][1] === 'x' && cells[2][0] === 'x') {
+        alert('x won');
+        hasWon = true;
+    } else if (cells[0][0] === 'o' && cells[0][1] === 'o' && cells[0][2] === 'o') { //rows o
+        alert('o won');
+        hasWon = true;
+    } else if (cells[1][0] === 'o' && cells[1][1] === 'o' && cells[1][2] === 'o') {
+        alert('o won');
+        hasWon = true;
+    } else if (cells[2][0] === 'o' && cells[2][1] === 'o' && cells[2][2] === 'o') {
+        alert('o won');
+        hasWon = true;
+    } else if (cells[0][0] === 'o' && cells[1][0] === 'o' && cells[2][0] === 'o') { //columns o
+        alert('o won');
+        hasWon = true;
+    } else if (cells[0][1] === 'o' && cells[1][1] === 'o' && cells[2][1] === 'o') {
+        alert('o won');
+        hasWon = true;
+    } else if (cells[0][2] === 'o' && cells[1][2] === 'o' && cells[2][2] === 'o') {
+        alert('o won');
+        hasWon = true;
+    } else if (cells[0][0] === 'o' && cells[1][1] === 'o' && cells[2][2] === 'o') {  //diagonal o
+        alert('o won');
+        hasWon = true;
+    } else if (cells[0][2] === 'o' && cells[1][1] === 'o' && cells[2][0] === 'o') {
+        alert('o won');
+        hasWon = true;
     }
-
+    if(hasWon){
+       //todo: resetGame();
+    }
 }
