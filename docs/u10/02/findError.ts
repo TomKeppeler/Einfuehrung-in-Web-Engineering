58898c3bed79b15060108a4bc2e51fc6b25ff167
Tom Keppeler <49951477+TomKeppeler@users.noreply.github.com>
2023-01-10 20:05:08 +0100

    work

---
+++ docs/u10/02/findError.ts
@@ -0,0 +1,9 @@
+const add = (x : number, y : number) => x + y;
+const equals = (x : any, y : any) => x === y;
+
+console.log( add(1,2) );
+console.log( add(true, true ) );
+console.log( add(true, false ) );
+let y : number;
+let x : number = y = 1; //y wurde nicht definiert
+console.log( add(equals(x,y), equals(y,x)) );
